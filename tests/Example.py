eb38699e159679fabd30ca5f531a0716160ef686
Albert <thegoversus@gmail.com>
2024-06-13 03:07:04 +0300

    добавлена статистика по гиверам

--- tests/Example.py
+++ tests/Example.py
@@ -1,4 +1,4 @@
-from tonapi import AnonGetter, TonApiGetter
+from tonapi import AnonGetter, GramGetter, TonApiGetter
 
 # AnonGetter - Кастомный интерфейс для ANON
 
@@ -11,8 +11,20 @@ from tonapi import AnonGetter, TonApiGetter
 # getter.get_rates()  # Возвращает цену в указанных валютах и изменение за 24ч и 7 дней
 # -> {'prices': {'TON': ..., 'USD': ...}, 'diff_24h': {'TON': ..., 'USD': ...}, 'diff_7d': {'TON': ..., 'USD': ...}}
 
-print("Цены:\n\t", AnonGetter.get_rates(), end='\n\n')
-print("Сожжено:\n\t", AnonGetter.get_burned_info(), end='\n\n')
-print("Кол-во холдеров:\n\t", AnonGetter.get_holders(), end='\n\n')
-print("Ликвидность в $:\n\t", AnonGetter.get_liquidity(), end='\n\n')
+# --- ANON ---
+# print("Цены:\n\t", AnonGetter.get_rates(), end='\n\n')
+# print("Сожжено:\n\t", AnonGetter.get_burned_info(), end='\n\n')
+# print("Кол-во холдеров:\n\t", AnonGetter.get_holders(), end='\n\n')
+# print("Ликвидность в $:\n\t", AnonGetter.get_liquidity(), end='\n\n')
 
+# --- GRAM ---
+print("Цены:\n\t", GramGetter.get_rates(), end='\n\n')
+print("Сожжено:\n\t", GramGetter.get_burned_info(), end='\n\n')
+print("Кол-во холдеров:\n\t", GramGetter.get_holders(), end='\n\n')
+print("Ликвидность в $:\n\t", GramGetter.get_liquidity(), end='\n\n')
+
+stats = GramGetter.get_givers_statistics()
+print("Суммарный баланс гиверов:\n\t", stats["balance_sum"], stats["stats"][0]["name"], end='\n\n')
+print("Статистика по гиверам:")
+for stat in stats["stats"]:
+    print("\t", stat)
--- tonapi/getter.py
+++ tonapi/getter.py
@@ -1,7 +1,7 @@
 """
 Ton API
 """
-import datetime
+import time
 
 import requests
 
@@ -11,18 +11,25 @@ EMISSION = 500_000_000
 
 
 class TonApiGetter:
-    def __init__(self, jetton: str, givers: list[str], currencies: list[str], burn_address: str = None):
+    def __init__(self, jetton: str, givers: list[str], currencies: list[str],
+                 burn_address: str = None, emission: int = None):
         """
         :param str jetton: Мастер-жетон искомой монеты
         :param list givers: Список адресов гиверов
         :param list currencies: Список валют - ['ton', 'usd', 'rub', ...]
+        :param int emission: эмиссия искомой монеты
         """
         self.jetton = jetton
         self.givers = givers
         self.currencies = currencies
         self.burn_address = burn_address
+        self.emission = emission
+
         if self.burn_address is None:
             self.burn_address = self._get_burn_addr()
+
+        if self.emission is None:
+            self.emission = EMISSION
 
     def _get_burn_addr(self) -> str:
         """
@@ -40,6 +47,24 @@ class TonApiGetter:
         response = requests.get(url)
         data = response.json()
         return data["asset"]["contract_address"]
+
+    def _get_giver_stat(self, giver_addr: str) -> dict:
+        """
+        Возвращает статистику по гиверу - адрес, название токена, баланс и состояние
+        :param str giver_addr: адрес гивера
+        :return dict:
+        """
+        url = f"{TON_API_URL}/accounts/{giver_addr}/jettons/{self.jetton}"
+
+        response = requests.get(url)
+        data = response.json()
+
+        return {
+            "address": giver_addr,
+            "name": data["jetton"]["name"],
+            "balance": int(data["balance"]) / 1000000000,
+            "condition": "working" if int(data["balance"]) >= 100000000000 else "drained"
+        }
 
     def get_meta(self) -> dict:
         """
@@ -82,7 +107,7 @@ class TonApiGetter:
         """
         burned = self.get_burned()
         rounded = burned / 1000000000
-        percentage = rounded * 100 / EMISSION
+        percentage = rounded * 100 / self.emission
         return {
             "raw": burned,
             "rounded": rounded,
@@ -106,6 +131,20 @@ class TonApiGetter:
         #  time_until = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S')
 
         raise NotImplementedError
+
+    def get_givers_statistics(self) -> dict:
+        """
+        Возвращает статистику по всем гиверам - адрес, название токена, баланс, состояние и общий баланс
+        :return list:
+        """
+        statistics = []
+        for giver in self.givers:
+            statistics.append(self._get_giver_stat(giver))
+            time.sleep(0.5)  # Ограничение для обхода лимитов бесплатного API
+        return {
+            "balance_sum": sum([elem["balance"] for elem in statistics]),
+            "stats": statistics
+        }
 
     def get_liquidity(self) -> float:
         """
